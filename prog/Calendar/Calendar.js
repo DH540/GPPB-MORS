269a7416c73bee90706c4ed67321679183aa791a
Monzikoy <rishon0520@gmail.com>
2025-03-18 23:14:47 +0800

    Updated responsiveness for calendar page (third try's a charm)

--- prog/Calendar/Calendar.js
+++ prog/Calendar/Calendar.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var calendarEl = document.getElementById('calendar');
     var calendar = new FullCalendar.Calendar(calendarEl, {
       initialView: 'dayGridMonth',
-      aspectRatio: 1.5,
+      aspectRatio: 2,
       headerToolbar: {
         left: 'prev,next today',
         center: 'title',
