d90805af22eebcb83b373a572223d33cc29e0baa
Monzikoy <rishon0520@gmail.com>
2025-03-18 21:38:42 +0800

    Added updates to calendar page :D

---
+++ prog/Calendar/Calendar.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      initialView: 'dayGridMonth',
+      contentHeight: 'auto',
+      headerToolbar: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,timeGridDay'
+      }
+    });
+    calendar.render();
+  });
