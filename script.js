469c8d7d306dee02f5443c4737aae61a17b6bd60
DH540 <dylanaguillosalazar@gmail.com>
2025-03-20 13:01:22 +0800

    commit all

--- script.js
+++ script.js
@@ -145,7 +145,7 @@ document.addEventListener('DOMContentLoaded', () => {
             message.textContent = "Login successful! Redirecting...";
 
             setTimeout(() => {
-                window.location.href = "../Inbox/inbox.html"; 
+                window.location.href = "prog/";
             }, 1000);
         } else {
             message.style.color = "red";
